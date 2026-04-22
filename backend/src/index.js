28db17fa4abb37e9b49cdecc512324ad2eb30022
DGLOliveira <dgloliveira90@gmail.com>
2026-04-22 14:58:45 +0000

    database setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 
 const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
     res.send("Hello from backend");
@@ -9,6 +15,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes)
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT);
+    connectDB();
 });
